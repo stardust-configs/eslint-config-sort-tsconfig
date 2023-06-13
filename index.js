802cbab69caf84fcddb01cbf2b4f627230cd968f
P-Chan <hello@0x50.io>
2023-06-14 04:47:41 +0900

    feat: add $schema

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
           {
             pathPattern: '^$',
             order: [
+              '$schema',
               'extends',
               'compilerOptions',
               'watchOptions',
